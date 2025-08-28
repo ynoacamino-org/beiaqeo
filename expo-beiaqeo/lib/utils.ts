77ae9619eb55386e85aaa0d7057e8b71f221ce40
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-08-28 18:57:03 -0500

    fix: format to remove linter errors

--- expo-beiaqeo/lib/utils.ts
+++ expo-beiaqeo/lib/utils.ts
@@ -1,12 +1,12 @@
-import { PocketBaseConfig } from "@/config/global"
-import { clsx, type ClassValue } from "clsx"
-import { AuthRecord } from "pocketbase"
-import { twMerge } from "tailwind-merge"
+import { PocketBaseConfig } from '@/config/global';
+import { clsx, type ClassValue } from 'clsx';
+import { AuthRecord } from 'pocketbase';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function extractAvatar(authRecord: AuthRecord): string {
-  return `${PocketBaseConfig.url}/api/files/${authRecord?.collectionId}/${authRecord?.id}/${authRecord?.avatar}`
+  return `${PocketBaseConfig.url}/api/files/${authRecord?.collectionId}/${authRecord?.id}/${authRecord?.avatar}`;
 }
