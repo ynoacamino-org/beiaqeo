ee20eca995bffd86ce8ba2293a4bab873214ae1c
gustadev24 <lgsc21211@gmail.com>
2025-08-28 17:02:23 -0500

    feat: try to use universal links to open the app

--- pocketbase/routes/routes.go
+++ pocketbase/routes/routes.go
@@ -3,7 +3,8 @@ package routes
 import "github.com/pocketbase/pocketbase/core"
 
 func Register(se *core.ServeEvent) error {
-	RedirectOAuth(se)
+	// RedirectOAuth(se)
+	WellKnown(se)
 
 	return se.Next()
 }
