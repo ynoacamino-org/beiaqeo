9d09da9a55b62f62ef51b18e2948d4ae6e5f76f8
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-08-29 09:25:28 -0500

    fix: remove google provider validator

---
+++ pocketbase/routes/auth_callback.go
@@ -0,0 +1,16 @@
+package routes
+
+import "github.com/pocketbase/pocketbase/core"
+
+func AuthCallback(se *core.ServeEvent) {
+	se.Router.GET("/auth/callback", func(e *core.RequestEvent) error {	
+		query := e.Request.URL.RawQuery
+
+		redirectURL := "beiaqeo://auth/callback"
+		if query != "" {
+			redirectURL += "?" + query
+		}
+
+		return e.Redirect(302, redirectURL)
+	})
+}
