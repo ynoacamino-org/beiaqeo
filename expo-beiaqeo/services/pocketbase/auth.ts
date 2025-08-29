f09bc4236559bb78e376ba9a620bf1d25ef1cb3a
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-08-29 12:29:08 -0500

    fix: change redirectURL to custom scheme

--- expo-beiaqeo/services/pocketbase/auth.ts
+++ expo-beiaqeo/services/pocketbase/auth.ts
@@ -2,17 +2,15 @@ import { pb } from '@/services/pocketbase/pb';
 import PocketBase, { AuthRecord, RecordAuthResponse } from 'pocketbase';
 import * as WebBrowser from 'expo-web-browser';
 import '@/config/eventsource';
-import { PocketBaseConfig } from '@/config/global';
 
 WebBrowser.maybeCompleteAuthSession();
 
 class PocketBaseAuthService {
   async loginWithGoogle(): Promise<RecordAuthResponse> {
-    const redirectUrl = `${PocketBaseConfig.url}/auth/callback`;
     const recordAuth = await pb.collection('users').authWithOAuth2({
       provider: 'google',
       urlCallback: async (url) => {
-        await WebBrowser.openAuthSessionAsync(url, redirectUrl).catch(console.error);
+        await WebBrowser.openAuthSessionAsync(url, 'beiaqeo://').catch(console.error);
       },
     });
     return recordAuth;
